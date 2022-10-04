eea3ee5535de3776a5f85728e6d72eae7f9dfc2e
mike <mikethemerchant@gmail.com>
2022-10-04 09:23:36 -0400

    gitRepoJS

---
+++ dogAge.js
@@ -0,0 +1,15 @@
+// this is my age
+var myAge = 42;
+// this is the dog years that count as 10.5
+var earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+// this give the dog years that are mutipled by 6
+laterYears = myAge - 2;
+// now mutiply
+laterYears *= 4;
+// my dog age
+myAgeInDogYears = earlyYears + laterYears;
+// lower case my name
+var myName = 'MIKE'.toLowerCase();
+// end game
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
